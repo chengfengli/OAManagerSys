9fedbc397dc3961b27f4e0dd5d58e04c97eb7996
RTF <RTF@RTF-PC>
2017-07-02 23:10:51 +0800

    拦截器

---
+++ src/main/java/com/oamanagersys/util/interceptor/LoginInterceptor.java
@@ -0,0 +1,43 @@
+package com.oamanagersys.util.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 登录拦截器
+ * @author 李明
+ * @time 2017年7月2日 下午10:29:03
+ * @phone 17310545652
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception e)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object, ModelAndView arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+		//获取Session  
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("user");
+        if(user != null){
+        	return true;
+        }else{
+        	//不符合条件的，跳转到登录界面  
+            request.getRequestDispatcher("pages/login/login.jsp").forward(request, response);
+        	return false;
+        }
+	}
+
+}
